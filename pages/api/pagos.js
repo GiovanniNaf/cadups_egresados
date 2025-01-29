d3441e619548208841d6bb33333fad373c5f8262
Giovanni Nafate <giovanninafate@MacBook-Air-de-Giovanni.local>
2025-01-29 13:01:39 -0600

    Primer commit: agrega los archivos iniciales

--- pages/api/pagos.js
+++ pages/api/pagos.js
@@ -1,4 +1,4 @@
-import { supabase } from '../../config/supabaseClient';
+import { supabase } from '@/utils/supabase.js';
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
